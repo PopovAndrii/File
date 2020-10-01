a5957a2e29abf3527e47a0ab5a4d4be6aeaa93cc
Popov Andreii <autoxarkov@gmail.com>
2020-10-01 06:58:08 -0700

    draft

--- FileCustom.cpp
+++ FileCustom.cpp
@@ -2,8 +2,7 @@
 
 void FileCustom::Copy(const char& from, const char& to) {
 
-	HANDLE hR = CreateFileA(
-		&from,
+	HANDLE hR = CreateFileA(&from,
 		GENERIC_WRITE | GENERIC_READ,
 		0,
 		NULL,
@@ -11,9 +10,9 @@ void FileCustom::Copy(const char& from, const char& to) {
 		FILE_ATTRIBUTE_NORMAL,
 		NULL
 	);
+	if (hR == INVALID_HANDLE_VALUE)	{ std::cout << "Could not create \"from\" file.\n"; }
 
-	HANDLE hW = CreateFileA(
-		&to,
+	HANDLE hW = CreateFileA(&to,
 		GENERIC_WRITE | GENERIC_READ,
 		0,
 		NULL,
@@ -21,11 +20,13 @@ void FileCustom::Copy(const char& from, const char& to) {
 		FILE_ATTRIBUTE_NORMAL,
 		NULL
 	);
+	if (hW == INVALID_HANDLE_VALUE)	{ std::cout << "Could not create temporary file.\n"; }
 
 	int bSize = GetFileSize(hR, 0);
 
 	int it = (bSize / m_nNumberOfBytesToRead) + 1;
 
+	/*
 	for (; it; --it, bSize -= m_nNumberOfBytesToRead) {
 
 		if (it == 1) m_nNumberOfBytesToRead = bSize;
@@ -59,7 +60,18 @@ void FileCustom::Copy(const char& from, const char& to) {
 		else {
 			//..can't read
 		}
-	}
+	} */
+
+	char buffer[4096];
+
+	do
+	{
+		if (ReadFile(hR, &buffer, 4096, &m_lpNumberOfBytesRead, NULL))
+		{
+			std::cout << buffer[0];
+			WriteFile(hW, &buffer, m_lpNumberOfBytesRead, NULL, NULL);
+		}
+	} while (m_lpNumberOfBytesRead == 4096);
 
 	CloseHandle(hR);
 	CloseHandle(hW);
